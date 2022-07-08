415a21117beb1e96b48bbc08fa341e13fad42d6f
Javier Suárez <javiersuarsan@gmail.com>
2022-07-08 19:20:35 +0200

    feat: add signin and customers components
    Add Signin and Customers page components, setting basic styles and cleaning project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './views/customers/customers.component';
+import { SigninComponent } from './views/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomersComponent, pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
