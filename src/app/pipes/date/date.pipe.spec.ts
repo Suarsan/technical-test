d600ac7df27aa081d966522b3685039fd3b51e14
Javier Suárez <javiersuarsan@gmail.com>
2022-07-09 18:45:52 +0200

    test: test date pipe
    Add some tests at date pipe

--- src/app/pipes/date/date.pipe.spec.ts
+++ src/app/pipes/date/date.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('DatePipe', () => {
     const pipe = new DatePipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return right date format', () => {
+    const pipe = new DatePipe();
+    const date = pipe.transform('Mon, 17 Jan 2022 19:51:28 GMT');
+    expect(date).toBe('17/01/2022 - 20:51h');
+  });
 });
