66c15f6280e92acb149e2c499c1f5743d4c1af9b
Javier Suárez <javiersuarsan@gmail.com>
2022-07-09 10:09:58 +0200

    feat: add 3000ms delay at login
    Add 3000ms delay at login request

--- src/app/services/user-services/user.service.ts
+++ src/app/services/user-services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, isDevMode } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map, tap, delay } from 'rxjs/operators';
 import { UserDaoService } from 'src/app/daos/user-daos/user-dao.service';
 import { UserModel } from 'src/app/models/user.model';
 
@@ -14,6 +14,7 @@ export class UserService {
   public signin(user: string, password: string): Observable<UserModel | any> {
     return this.userDaoService.signin(user, password).pipe(
       tap((o: UserModel) => !isDevMode() || console.dir(o)),
+      delay(3000),
       map((o: UserModel) => o),
       catchError(error => { console.dir(error); return error; })
     );
