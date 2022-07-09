aac518256a139e40c76587be3e8970f882f86f4a
Javier Suárez <javiersuarsan@gmail.com>
2022-07-09 18:46:24 +0200

    test: test name initials pipe
    Add some tests at name initials pipe

--- src/app/pipes/name-initials/name-initials.pipe.spec.ts
+++ src/app/pipes/name-initials/name-initials.pipe.spec.ts
@@ -1,8 +1,35 @@
 import { NameInitialsPipe } from './name-initials.pipe';
 
 describe('NameInitialsPipe', () => {
+  
   it('create an instance', () => {
     const pipe = new NameInitialsPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should returns 2 letters', () => {
+    const pipe = new NameInitialsPipe();
+    const initials = pipe.transform('Javier Suárez', 2) as string;
+    expect(typeof initials).toBe('string');
+    expect(initials.length).toBe(2);
+  })
+  it('should returns 2 letters', () => {
+    const pipe = new NameInitialsPipe();
+    const initials = pipe.transform('Javier Suárez Sánchez', 2) as string;
+    expect(typeof initials).toBe('string');
+    expect(initials.length).toBe(2)
+  })
+
+  it('should returns 3 letters', () => {
+    const pipe = new NameInitialsPipe();
+    const initials = pipe.transform('Javier Suárez Sánchez', 3) as string;
+    expect(typeof initials).toBe('string');
+    expect(initials.length).toBe(3)
+  })
+  it('should returns 3 letters', () => {
+    const pipe = new NameInitialsPipe();
+    const initials = pipe.transform('Javier Suárez Sánchez', 5) as string;
+    expect(typeof initials).toBe('string');
+    expect(initials.length).toBe(3)
+  })
 });
